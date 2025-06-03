61b2d9c356c9bbc285ea800235195cd09335245d
zhaoguangxu <zhaoguangxu801@gmail.com>
2025-06-03 19:10:53 +0800

    chore: 更新 Git 提交规范，添加 lint-staged 配置和 Husky 钩子

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -5,17 +5,17 @@ module.exports = {
       2,
       'always',
       [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'chore',
-        'revert',
-        'build',
-        'ci',
+        'feat', // 新功能
+        'fix', // 修复
+        'docs', // 文档变更
+        'style', // 代码格式（不影响代码运行的变动）
+        'refactor', // 重构（既不是新增功能，也不是修改 bug 的代码变动）
+        'perf', // 性能优化
+        'test', // 增加测试
+        'chore', // 构建过程或辅助工具的变动
+        'revert', // 回退
+        'build', // 打包
+        'ci', // CI 配置相关
       ],
     ],
     'type-case': [0],
@@ -26,4 +26,4 @@ module.exports = {
     'subject-case': [0, 'never'],
     'header-max-length': [0, 'always', 72],
   },
-};
+}
